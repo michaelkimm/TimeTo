291ced016a0e6ad9906609443bc77affb71038ea
michael <mskim_451@dongguk.edu>
2023-06-08 20:06:59 +0900

    feature: 이번 달, 저번 달, 다음 달 구현

--- app/src/main/java/com/tt/timeto/MainActivity.kt
+++ app/src/main/java/com/tt/timeto/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.tt.timeto
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
+import com.tt.timeto.databinding.ActivityMainBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    
+    // 년월 변수
+    private lateinit var selectedDate: LocalDate 
+    
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        // binding 초기화
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        
+        // 현재 날짜
+        selectedDate = LocalDate.now()
+        
+        // 화면 설정
+        setMonthView()
+        
+        // 이전 달 버튼 이벤트
+        binding.preBtn.setOnClickListener {
+            // 현재 월 -1 변수에 담기
+            selectedDate = selectedDate.minusMonths(1)
+            setMonthView()
+        }
+        
+        // 다음 달 버튼 이벤트
+        binding.nextBtn.setOnClickListener {
+            selectedDate = selectedDate.plusMonths(1)
+            setMonthView()
+        }
+    }
+
+    // 날짜 화면에 보여주기
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setMonthView() {
+        // 년월 텍스트뷰 셋팅
+         binding.monthYearText.text = monthYearFromDate(selectedDate)
+    }
+
+    // 잘짜 타입 설정
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun monthYearFromDate(date: LocalDate): String {
+        var formatter = DateTimeFormatter.ofPattern("MM월 yyyy")
+        
+        // 받아온 날짜를 해당 포맷으로 변경
+        return date.format(formatter)
     }
 }
